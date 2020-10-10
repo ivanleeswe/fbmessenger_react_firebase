d3a40429923de7d12b33c405cc43b3faef8f9622
Ivan Lee <ilee.code@gmail.com>
2020-10-10 14:49:52 -0700

    Initalize create-react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Hello</h1>
+      <input />
+      <button>Send Message</button>
+    </div>
+  );
+}
+
+export default App;
