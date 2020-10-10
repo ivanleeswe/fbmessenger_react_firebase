1a2683467edcf211c1fa533888581c4461bdc985
Ivan Lee <ilee.code@gmail.com>
2020-10-10 15:28:01 -0700

    Build form to include input field and button. Implement functionto render message list

--- src/App.js
+++ src/App.js
@@ -3,16 +3,29 @@ import './App.css';
 
 function App() {
   const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
 
   const sendMessage = event => {
-
+    event.preventDefault();
+    setMessages([...messages, input]);
+    setInput('');
   }
 
   return (
     <div className="App">
       <h1>Hello Hello</h1>
-      <input value={input} onChange={event => setInput(event.target.value)}/>
-      <button onClick={sendMessage}>Send Message</button>
+
+      <form>
+        <input value={input} onChange={event => setInput(event.target.value)}/>
+        <button type='submit' onClick={sendMessage}>Send Message</button>
+      </form>
+ 
+
+      {
+        messages.map(message => (
+          <p>{message}</p>
+        ))
+      }
     </div>
   );
 }
