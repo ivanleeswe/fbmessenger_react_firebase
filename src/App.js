bad5dd339c42746d6f77796fedcb66a2647549fb
Ivan Lee <ilee.code@gmail.com>
2020-10-10 15:09:47 -0700

    Implement input field

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+
+  const sendMessage = event => {
+
+  }
+
   return (
     <div className="App">
       <h1>Hello Hello</h1>
-      <input />
-      <button>Send Message</button>
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <button onClick={sendMessage}>Send Message</button>
     </div>
   );
 }
